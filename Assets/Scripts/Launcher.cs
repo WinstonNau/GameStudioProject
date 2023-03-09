8549160a906147f18c360f1b84d33945fc15ac14
Winston <winstonnau007@gmail.com>
2023-03-09 14:03:38 +0100

    :heavy_plus_sign: added photon library

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("connecting to master");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("connected to master");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        MenuManager.Instance.OpenMenu("title");
+        Debug.Log("joined lobby");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
